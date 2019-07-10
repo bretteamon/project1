030633ca6e65f91cd9927c078b2f898656c9a033
bretteamon <21160038+bretteamon@users.noreply.github.com>
2019-07-09 19:11:37 -0700

    Changes

--- script/api.js
+++ script/api.js
@@ -25,23 +25,27 @@
 
 /*#endregion--------------------------------------------*/
 /*#region---------Cycle Thru Cards----------------------*/
-const mtg = require('mtgsdk');
+const mtg = require('mtgsdk')
 
+mtg.card.find(2)
+  .then(result => {
+    console.log(result.card.name) // "Black Lotus"
+})
 
-const getCard = () => {
-  const xhr = new XMLHttpRequest();
-  const set = '?set=XLN&set=RIX&';
-  const creatureType = document.getElementById('creatureType').value;
-  const url = 'https://api.magicthegathering.io/v1/cards';
-  const urlComplete = `${url}${set}${creatureType}`;
-  xhr.onreadystatechange = () => {
-    if(xhr.readyState === XMLHttpRequest.DONE){ 
-      console.log(xhr.response);
-        results = xhr.response;
-    }
-  };
-  xhr.open('GET',urlComplete);
-  xhr.send();
-};
+// const getCard = () => {
+//   const xhr = new XMLHttpRequest();
+//   const set = '?set=XLN&set=RIX&';
+//   const creatureType = document.getElementById('creatureType').value;
+//   const url = 'https://api.magicthegathering.io/v1/cards';
+//   const urlComplete = `${url}${set}${creatureType}`;
+//   xhr.onreadystatechange = () => {
+//     if(xhr.readyState === XMLHttpRequest.DONE){ 
+//       console.log(xhr.response);
+//         results = xhr.response;
+//     }
+//   };
+//   xhr.open('GET',urlComplete);
+//   xhr.send();
+// };
 
-document.getElementById('creatureType').addEventListener('change',getCard);
+// document.getElementById('creatureType').addEventListener('change',getCard);
